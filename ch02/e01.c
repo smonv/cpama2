be600eb3ee6d916fd35e789b25542105668a92d7
Tien Thanh <tthanh.smt@gmail.com>
2017-12-22 16:38:00 +0700

    ch02/e01: reformat

--- ch02/e01.c
+++ ch02/e01.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
     printf("        *\n");
-	printf("       *\n");
-	printf("      *\n");
-	printf("     *\n");
-	printf("*   *\n");
-	printf(" * *\n");
-	printf("  *\n");
+    printf("       *\n");
+    printf("      *\n");
+    printf("     *\n");
+    printf("*   *\n");
+    printf(" * *\n");
+    printf("  *\n");
 
     return 0;
 }
